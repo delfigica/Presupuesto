5ad084cb4035c60fa70bc2f3e7ef93733714d603
delfigica <delfinamariacanas@gmail.com>
2021-09-06 12:45:27 -0300

    first upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PresupuestoComponent } from './presupuesto/presupuesto.component';
-import { IngresoComponent } from './ingreso/ingreso.component';
-import { EgresoComponent } from './egreso/egreso.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { EgresoServicio } from './egreso/egreso.servicio';
-import { IngresoServicio } from './ingreso/ingreso.servicio';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PresupuestoComponent,
-    IngresoComponent,
-    EgresoComponent,
-    FormularioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [IngresoServicio, EgresoServicio],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
